1042cf963f872a0011267f51d239c7c39fb18101
octavio <octavio@octavios-MacBook-Air.local>
2024-09-24 17:47:52 -0300

    Update realizado na data Tue Sep 24 17:47:52 -03 2024

--- Essenciais - C/soma.c
+++ Essenciais - C/soma.c
@@ -1,11 +1,22 @@
+/*
+Esse código realzia a soma de dois números inteiros
+*/
+// inclusão da biblioteca padrao Standard IO
 #include <stdio.h>
 
+
+
 int main(){
+	//Declaração e inicialização das variáveis num1 e num2.
 	int num1 = 0;
 	int num2 = 0;
+
 	printf("Digite o primeiro número: ");
 	scanf("%d", &num1);
+
 	printf("Digite o segundo número: ");
 	scanf("%d",&num2);
+
+	//Print do resultado
 	printf("Soma = %d\n", num1+num2 );
 }
